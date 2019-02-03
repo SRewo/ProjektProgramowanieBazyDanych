29aa5c1a1994bc0f20b13f7253fd4e5ff83d02ef
Mateusz-Slisz <mat.slisz@yahoo.com>
2019-02-03 21:33:22 +0100

    fix grids, add update on guests list

--- ProjektHotel/Goscie.xaml.cs
+++ ProjektHotel/Goscie.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data;
+using System.Data.SqlClient;
 using MahApps.Metro.Controls;
 
 namespace ProjektHotel
@@ -22,6 +23,7 @@ namespace ProjektHotel
     public partial class Goscie : MetroWindow
     {
         private DataSet _dataSet;
+        private SqlDataAdapter _adapter;
 
         public Goscie()
         {
@@ -30,16 +32,42 @@ namespace ProjektHotel
             Owner = Application.Current.MainWindow;
         }
 
-        public Goscie(DataSet dataSet) : this()
+        public Goscie(DataSet dataSet, SqlDataAdapter adapter) : this()
         {
             _dataSet = dataSet;
+            _adapter = adapter;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            reloadGoscie();
             DataTable goscieTabela = _dataSet.Tables["goscie"];
             GoscieGrid.ItemsSource = goscieTabela.DefaultView;
+
+        }
+
+        private void Btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            SqlCommandBuilder updateCommand = new SqlCommandBuilder(_adapter);
+            try
+            {
+                _adapter.Update(_dataSet, "goscie");
+                MessageBox.Show("Pomyślnie zaktualizowane dane.");
+            }
+            catch (SqlException)
+            {
+
+                MessageBox.Show("Podano nieprawidłowe dane!");
+            }
+        }
+
+        private void reloadGoscie()
+        {
+
+            DataTable goscie = _dataSet.Tables["goscie"];
+            _dataSet.Tables.Remove(goscie);
+            _adapter.SelectCommand.CommandText = "SELECT * FROM goscie";
+            _adapter.Fill(_dataSet, "goscie");
 
         }
     }
